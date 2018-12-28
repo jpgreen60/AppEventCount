e354b1d505f9b8285a8399c149a308922adfc075
JohnPGreen <johnpgreen60@gmail.com>
2018-12-28 01:09:43 -0500

    Initial Commit

---
+++ AppEventCount/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppEventCount
+//
+//  Created by John Green on -12-282018.
+//  Copyright © 2018 John Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
