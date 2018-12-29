594a0a4c6df9fbca45b04ba95be38f8697efb3a8
JohnPGreen <johnpgreen60@gmail.com>
2018-12-29 00:23:46 -0500

    Part 1

--- AppEventCount/ViewController.swift
+++ AppEventCount/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var launchCount = 0
+    var resignCount = 0
+    var backgroundCount = 0
+    var foregroundCount = 0
+    var activeCount = 0
+    var terminateCount = 0
+    
+    
+    @IBOutlet weak var didFinishLaunchingWithOptionsLabel: UILabel!
+    @IBOutlet weak var applicationWillResignActiveLabel: UILabel!
+    @IBOutlet weak var applicationDidEnterBackgroundLabel: UILabel!
+    @IBOutlet weak var applicationWillEnterForegroundLabel: UILabel!
+     @IBOutlet weak var applicationDidBecomeActiveLabel: UILabel!
+    @IBOutlet weak var applicationWillTerminateLabel: UILabel!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateView()
     }
 
-
+    
+    func updateView() {
+        
+        didFinishLaunchingWithOptionsLabel.text = "The app has launched \(launchCount) time(s)"
+        
+        applicationWillResignActiveLabel.text = "The app has resigned \(resignCount) time(s)"
+        
+        applicationDidEnterBackgroundLabel.text = "The app has entered the background \(backgroundCount) time(s)"
+        
+        applicationWillEnterForegroundLabel.text = "The app has entered the foreground \(foregroundCount) time(s)"
+        
+        applicationDidBecomeActiveLabel.text = "The app has become active \(activeCount) time(s)"
+        
+        applicationWillTerminateLabel.text = "The app has terminated \(terminateCount) time(s)"
+        
+    }
+    
+    
 }
 
